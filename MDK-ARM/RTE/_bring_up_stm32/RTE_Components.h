55d2b2b2692fddc21edddd6fba4f8b34f076ce47
wayneyao <417838124@qq.com>
2024-01-14 18:07:17 +0800

    1：first commit

---
+++ MDK-ARM/RTE/_bring_up_stm32/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'bring_up_stm32' 
+ * Target:  'bring_up_stm32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
